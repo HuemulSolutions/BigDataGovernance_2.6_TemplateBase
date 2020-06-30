75032aba26725d479406f76ba9e5493769d6bfec
Sebastián Rodríguez Robotham <sebas_rod@hotmail.com>
2020-06-29 22:04:49 -0400

    version 2.6, upgrade versiones

--- src/main/scala/com/yourcompany/yourapplication/globalSettings.scala
+++ src/main/scala/com/yourcompany/yourapplication/globalSettings.scala
@@ -17,7 +17,7 @@ object globalSettings {
    
    //Local directory to open configuration files
    val localPath: String = System.getProperty("user.dir").concat("/")
-   println(s"path: ${localPath}")
+   println(s"path: $localPath")
    
    /**
    * Get encrypted key from file, and return decrypted key. 
@@ -30,12 +30,12 @@ object globalSettings {
       key = openFile.getLines.mkString
       openFile.close()
     } catch {
-        case e: FileNotFoundException => println(s"Couldn't find that file: ${fileName}")
-        case e: IOException => println(s"(${fileName}). Got an IOException! ${e.getLocalizedMessage}")
-        case e: Exception => println(s"exception opening ${fileName}")
+        case _: FileNotFoundException => println(s"Couldn't find that file: $fileName")
+        case e: IOException => println(s"($fileName). Got an IOException! ${e.getLocalizedMessage}")
+        case _: Exception => println(s"exception opening $fileName")
     }
     
-    return key
+    key
   }
    
 
@@ -80,38 +80,38 @@ object globalSettings {
    
    //BACKUP
    Global.MDM_SaveBackup = true
-   Global.MDM_Backup_Path.append(new huemul_KeyValuePath("production",s"${baseDir}/production/backup/"))
-   Global.MDM_Backup_Path.append(new huemul_KeyValuePath("experimental",s"${baseDir}/experimental/backup/"))
+   Global.MDM_Backup_Path.append(new huemul_KeyValuePath("production",s"$baseDir/production/backup/"))
+   Global.MDM_Backup_Path.append(new huemul_KeyValuePath("experimental",s"$baseDir/experimental/backup/"))
       
    //DATA QUALITY PATH & DATABASE
    Global.DQ_SaveErrorDetails = true
    Global.DQError_DataBase.append(new huemul_KeyValuePath("production","production_master"))   
    Global.DQError_DataBase.append(new huemul_KeyValuePath("experimental","experimental_master"))
    
-   Global.DQError_Path.append(new huemul_KeyValuePath("production",s"${baseDir}/production/dq/"))
-   Global.DQError_Path.append(new huemul_KeyValuePath("experimental",s"${baseDir}/experimental/dq/"))
-   
+   Global.DQError_Path.append(new huemul_KeyValuePath("production",s"$baseDir/production/dq/"))
+   Global.DQError_Path.append(new huemul_KeyValuePath("experimental",s"$baseDir/experimental/dq/"))
+
    //MDM OLD VALUE TRACE PATH & DATABASE
    Global.MDM_SaveOldValueTrace = true
    Global.MDM_OldValueTrace_DataBase.append(new huemul_KeyValuePath("production","production_oldvalue"))   
    Global.MDM_OldValueTrace_DataBase.append(new huemul_KeyValuePath("experimental","experimental_oldvalue"))
    
-   Global.MDM_OldValueTrace_Path.append(new huemul_KeyValuePath("production",s"${baseDir}/production/oldvalue/"))
-   Global.MDM_OldValueTrace_Path.append(new huemul_KeyValuePath("experimental",s"${baseDir}/experimental/oldvalue/"))
+   Global.MDM_OldValueTrace_Path.append(new huemul_KeyValuePath("production",s"$baseDir/production/oldvalue/"))
+   Global.MDM_OldValueTrace_Path.append(new huemul_KeyValuePath("experimental",s"$baseDir/experimental/oldvalue/"))
    
    /**
     *FROM 1.0 
     */
    //TEMPORAL SETTING
-   Global.TEMPORAL_Path.append(new huemul_KeyValuePath("production",s"${baseDir}/production/temp/"))
-   Global.TEMPORAL_Path.append(new huemul_KeyValuePath("experimental",s"${baseDir}/experimental/temp/"))
+   Global.TEMPORAL_Path.append(new huemul_KeyValuePath("production",s"$baseDir/production/temp/"))
+   Global.TEMPORAL_Path.append(new huemul_KeyValuePath("experimental",s"$baseDir/experimental/temp/"))
      
    //RAW SETTING
-   Global.RAW_SmallFiles_Path.append(new huemul_KeyValuePath("production",s"${baseDir}/production/raw/"))
-   Global.RAW_SmallFiles_Path.append(new huemul_KeyValuePath("experimental",s"${baseDir}/experimental/raw/"))
+   Global.RAW_SmallFiles_Path.append(new huemul_KeyValuePath("production",s"$baseDir/production/raw/"))
+   Global.RAW_SmallFiles_Path.append(new huemul_KeyValuePath("experimental",s"$baseDir/experimental/raw/"))
    
-   Global.RAW_BigFiles_Path.append(new huemul_KeyValuePath("production",s"${baseDir}/production/raw/"))
-   Global.RAW_BigFiles_Path.append(new huemul_KeyValuePath("experimental",s"${baseDir}/experimental/raw/"))
+   Global.RAW_BigFiles_Path.append(new huemul_KeyValuePath("production",s"$baseDir/production/raw/"))
+   Global.RAW_BigFiles_Path.append(new huemul_KeyValuePath("experimental",s"$baseDir/experimental/raw/"))
    
    
    
@@ -119,51 +119,51 @@ object globalSettings {
    Global.MASTER_DataBase.append(new huemul_KeyValuePath("production","production_master"))   
    Global.MASTER_DataBase.append(new huemul_KeyValuePath("experimental","experimental_master"))
 
-   Global.MASTER_SmallFiles_Path.append(new huemul_KeyValuePath("production",s"${baseDir}/production/master/"))
-   Global.MASTER_SmallFiles_Path.append(new huemul_KeyValuePath("experimental",s"${baseDir}/experimental/master/"))
+   Global.MASTER_SmallFiles_Path.append(new huemul_KeyValuePath("production",s"$baseDir/production/master/"))
+   Global.MASTER_SmallFiles_Path.append(new huemul_KeyValuePath("experimental",s"$baseDir/experimental/master/"))
    
-   Global.MASTER_BigFiles_Path.append(new huemul_KeyValuePath("production",s"${baseDir}/production/master/"))
-   Global.MASTER_BigFiles_Path.append(new huemul_KeyValuePath("experimental",s"${baseDir}/experimental/master/"))
+   Global.MASTER_BigFiles_Path.append(new huemul_KeyValuePath("production",s"$baseDir/production/master/"))
+   Global.MASTER_BigFiles_Path.append(new huemul_KeyValuePath("experimental",s"$baseDir/experimental/master/"))
 
    //DIM SETTING
    Global.DIM_DataBase.append(new huemul_KeyValuePath("production","production_dim"))   
    Global.DIM_DataBase.append(new huemul_KeyValuePath("experimental","experimental_dim"))
 
-   Global.DIM_SmallFiles_Path.append(new huemul_KeyValuePath("production",s"${baseDir}/production/dim/"))
-   Global.DIM_SmallFiles_Path.append(new huemul_KeyValuePath("experimental",s"${baseDir}/experimental/dim/"))
+   Global.DIM_SmallFiles_Path.append(new huemul_KeyValuePath("production",s"$baseDir/production/dim/"))
+   Global.DIM_SmallFiles_Path.append(new huemul_KeyValuePath("experimental",s"$baseDir/experimental/dim/"))
    
-   Global.DIM_BigFiles_Path.append(new huemul_KeyValuePath("production",s"${baseDir}/production/dim/"))
-   Global.DIM_BigFiles_Path.append(new huemul_KeyValuePath("experimental",s"${baseDir}/experimental/dim/"))
+   Global.DIM_BigFiles_Path.append(new huemul_KeyValuePath("production",s"$baseDir/production/dim/"))
+   Global.DIM_BigFiles_Path.append(new huemul_KeyValuePath("experimental",s"$baseDir/experimental/dim/"))
 
    //ANALYTICS SETTING
    Global.ANALYTICS_DataBase.append(new huemul_KeyValuePath("production","production_analytics"))   
    Global.ANALYTICS_DataBase.append(new huemul_KeyValuePath("experimental","experimental_analytics"))
    
-   Global.ANALYTICS_SmallFiles_Path.append(new huemul_KeyValuePath("production",s"${baseDir}/production/analytics/"))
-   Global.ANALYTICS_SmallFiles_Path.append(new huemul_KeyValuePath("experimental",s"${baseDir}/experimental/analytics/"))
+   Global.ANALYTICS_SmallFiles_Path.append(new huemul_KeyValuePath("production",s"$baseDir/production/analytics/"))
+   Global.ANALYTICS_SmallFiles_Path.append(new huemul_KeyValuePath("experimental",s"$baseDir/experimental/analytics/"))
    
-   Global.ANALYTICS_BigFiles_Path.append(new huemul_KeyValuePath("production",s"${baseDir}/production/analytics/"))
-   Global.ANALYTICS_BigFiles_Path.append(new huemul_KeyValuePath("experimental",s"${baseDir}/experimental/analytics/"))
+   Global.ANALYTICS_BigFiles_Path.append(new huemul_KeyValuePath("production",s"$baseDir/production/analytics/"))
+   Global.ANALYTICS_BigFiles_Path.append(new huemul_KeyValuePath("experimental",s"$baseDir/experimental/analytics/"))
 
    //REPORTING SETTING
    Global.REPORTING_DataBase.append(new huemul_KeyValuePath("production","production_reporting"))
    Global.REPORTING_DataBase.append(new huemul_KeyValuePath("experimental","experimental_reporting"))
 
-   Global.REPORTING_SmallFiles_Path.append(new huemul_KeyValuePath("production",s"${baseDir}/production/reporting/"))
-   Global.REPORTING_SmallFiles_Path.append(new huemul_KeyValuePath("experimental",s"${baseDir}/experimental/reporting/"))
+   Global.REPORTING_SmallFiles_Path.append(new huemul_KeyValuePath("production",s"$baseDir/production/reporting/"))
+   Global.REPORTING_SmallFiles_Path.append(new huemul_KeyValuePath("experimental",s"$baseDir/experimental/reporting/"))
    
-   Global.REPORTING_BigFiles_Path.append(new huemul_KeyValuePath("production",s"${baseDir}/production/reporting/"))
-   Global.REPORTING_BigFiles_Path.append(new huemul_KeyValuePath("experimental",s"${baseDir}/experimental/reporting/"))
+   Global.REPORTING_BigFiles_Path.append(new huemul_KeyValuePath("production",s"$baseDir/production/reporting/"))
+   Global.REPORTING_BigFiles_Path.append(new huemul_KeyValuePath("experimental",s"$baseDir/experimental/reporting/"))
 
    //SANDBOX SETTING
    Global.SANDBOX_DataBase.append(new huemul_KeyValuePath("production","production_sandbox"))
    Global.SANDBOX_DataBase.append(new huemul_KeyValuePath("experimental","experimental_sandbox"))
    
-   Global.SANDBOX_SmallFiles_Path.append(new huemul_KeyValuePath("production",s"${baseDir}/production/sandbox/"))
-   Global.SANDBOX_SmallFiles_Path.append(new huemul_KeyValuePath("experimental",s"${baseDir}/experimental/sandbox/"))
+   Global.SANDBOX_SmallFiles_Path.append(new huemul_KeyValuePath("production",s"$baseDir/production/sandbox/"))
+   Global.SANDBOX_SmallFiles_Path.append(new huemul_KeyValuePath("experimental",s"$baseDir/experimental/sandbox/"))
    
-   Global.SANDBOX_BigFiles_Path.append(new huemul_KeyValuePath("production",s"${baseDir}/production/sandbox/"))
-   Global.SANDBOX_BigFiles_Path.append(new huemul_KeyValuePath("experimental",s"${baseDir}/experimental/sandbox/"))
+   Global.SANDBOX_BigFiles_Path.append(new huemul_KeyValuePath("production",s"$baseDir/production/sandbox/"))
+   Global.SANDBOX_BigFiles_Path.append(new huemul_KeyValuePath("experimental",s"$baseDir/experimental/sandbox/"))
 
    
 }
